275bf56180a23c9d68292ef9a43f2c8461f927e0
jameskyokushin <james@kyokushin.com.ph>
2012-03-23 14:02:08 +0800

    done excersize on chapter 7 test pass

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -27,6 +27,13 @@ describe "User pages" do
       it "should not create a user" do
         expect { click_button "Create my account" }.not_to change(User, :count)
       end
+
+      describe "error messages" do
+        before { click_button "Create my account" }
+
+        it { should have_selector('title', text: 'Sign up') }
+        it { should have_content('error') }
+      end
     end
 
     describe "with valid information" do
@@ -35,6 +42,14 @@ describe "User pages" do
         fill_in "Email",        with: "user@example.com"
         fill_in "Password",     with: "foobar"
         fill_in "Confirmation", with: "foobar"
+      end
+
+      describe "after saving the user" do
+        before { click_button "Create my account" }
+        let(:user) { User.find_by_email('user@example.com') }
+
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
 
       it "should create a user" do
