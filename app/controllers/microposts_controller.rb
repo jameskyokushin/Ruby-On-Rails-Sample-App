af4b3aec61b44e225b13c9c4fab613005eb07a60
jameskyokushin <james@kyokushin.com.ph>
2012-03-27 14:09:57 +0800

    user can able to post anything test pass

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -2,6 +2,13 @@ class MicropostsController < ApplicationController
   before_filter :signed_in_user
 
   def create
+    @micropost = current_user.microposts.build(params[:micropost])
+    if @micropost.save
+      flash[:success] = "Micropost Created."
+      redirect_to root_path
+    else
+      render 'static_pages/home'
+    end
   end
 
   def destroy
