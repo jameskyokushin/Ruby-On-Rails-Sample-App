805c098b73a82e1c4201dac49502b10921be1307
jameskyokushin <james@kyokushin.com.ph>
2012-03-28 16:15:30 +0800

    flash notice change to about gravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -35,7 +35,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome To ASI.COM.PH"
+      flash[:success] = "Your are now Successfully Sign in please change your gravatar."
       redirect_to @user
     else
       render 'new'
