bd8edb9da8ba8c7ea78173d1e3eba3d2ee2ded68
jameskyokushin <james@kyokushin.com.ph>
2012-03-28 14:06:38 +0800

    added follow form/follow/unfollow/stats test pass

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 SampleApp::Application.routes.draw do
-  resources :microposts, only:  [:create, :destroy]
-  resources :sessions,   only:  [:new, :create, :destroy]
-  resources :users  
-  
+  resources :sessions,      only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
   match '/signout', to: 'sessions#destroy', via: :delete
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,6 +20,33 @@ describe "Static pages" do
     describe "for signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
       before do
+        FactoryGirl.create(:micropost, :user => user, :content => "Lorem")
+        FactoryGirl.create(:micropost, :user => user, :content => "Ipsum")
+        sign_in user
+        visit root_path
+      end
+
+      it "should render the user's feed" do
+        user.feed.each do |item|
+          page.should have_selector("li##{item.id}", text: item.content)
+        end
+      end
+
+      describe "follower/following counts" do
+        let(:other_user) { FactoryGirl.create(:user) }
+        before do
+          other_user.follow!(user)
+          visit root_path
+        end
+
+        it { should have_link("0 following", href: following_user_path(user)) }
+        it { should have_link("1 follower",  href: followers_user_path(user)) }
+      end
+    end
+    
+    describe "for signed-in users" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
         FactoryGirl.create(:micropost, user: user, content: "Lorem ipsum")
         FactoryGirl.create(:micropost, user: user, content: "Dolor sit amet")
         sign_in user
