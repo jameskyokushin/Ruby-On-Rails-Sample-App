d9bc57799d1fadec4496babd6c5ffabadc36cad0
jameskyokushin <james@kyokushin.com.ph>
2012-03-28 14:53:53 +0800

    test failed ajax working well resume to 11.3

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -1,33 +1,40 @@
 require 'spec_helper'
 
-describe Relationship do
+describe RelationshipsController do
 
-  let(:follower) { FactoryGirl.create(:user) }
-  let(:followed) { FactoryGirl.create(:user) }
-  let(:relationship) do
-    follower.relationships.build(followed_id: followed.id)
+  let(:user) { FactoryGirl.create(:user) }
+  let(:other_user) { FactoryGirl.create(:user) }
+
+  before { sign_in user }
+
+  describe "creating a relationship with Ajax" do
+
+    it "should increment the Relationship count" do
+      expect do
+        xhr :post, :create, relationship: { followed_id: other_user.id }
+      end.should change(Relationship, :count).by(1)
+    end
+
+    it "should respond with success" do
+      xhr :post, :create, relationship: { followed_id: other_user.id }
+      response.should be_success
+    end
   end
 
-  subject { relationship }
+  describe "destroying a relationship with Ajax" do
 
-  it { should be_valid }
+    before { user.follow!(other_user) }
+    let(:relationship) { user.relationships.find_by_followed_id(other_user) }
 
-  describe "follower methods" do
-    before { relationship.save }
+    it "should decrement the Relationship count" do
+      expect do
+        xhr :delete, :destroy, id: relationship.id
+      end.should change(Relationship, :count).by(-1)
+    end
 
-    it { should respond_to(:follower) }
-    it { should respond_to(:followed) }
-    its(:follower) { should == follower }
-    its(:followed) { should == followed }
-  end
- 
-  describe "when followed id is not present" do
-    before { relationship.followed_id = nil }
-    it { should_not be_valid }
-  end
-
-  describe "when follower id is not present" do
-    before { relationship.follower_id = nil }
-    it { should_not be_valid }
+    it "should respond with success" do
+      xhr :delete, :destroy, id: relationship.id
+      response.should be_success
+    end
   end
 end
